10d85c83b0a6e3d72a7683d23ad8d469593a6c57
mkdir700 <mkdir700@gmail.com>
2020-06-13 17:20:26 +0800

    增加全局分页样式

--- marilyn/settings.py
+++ marilyn/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'coreapi',
     'corsheaders',
+    'django_filters',
     'options',
     'users',
     'metas',
@@ -167,4 +168,6 @@ REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
     # )
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
