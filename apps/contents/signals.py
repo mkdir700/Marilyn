e09f379051432b75779471fed36a9904a009cead
mkdir700 <mkdir700@gmail.com>
2020-06-16 23:24:49 +0800

    Fix: PUT更新文章,相关分类文章数量字段更新异常

--- apps/contents/signals.py
+++ apps/contents/signals.py
@@ -4,13 +4,31 @@ from contents.models import ContentsModel, RelationshipsModel
 from metas.models import MetasModel
 
 
+def add_count(metas):
+    for meta in metas:
+        meta.count += 1
+        meta.save()
+
+
+def sub_count(metas):
+    for meta in metas:
+        meta.count -= 1
+        meta.save()
+
+
 @receiver(m2m_changed, sender=RelationshipsModel, dispatch_uid="create_content")
 def create_content(sender, instance, action, pk_set, **kwargs):
-    if action == "post_add":
-        metas = MetasModel.objects.filter(meta_list__cid=instance.cid)
-        for meta in metas:
-            meta.count += 1
-            meta.save()
+    print(action, pk_set, type(pk_set))
+    # metas = MetasModel.objects.filter(meta_list__cid=instance.cid)
+    metas = MetasModel.objects.filter(mid__in=pk_set)
+    if action == "pre_remove":
+        sub_count(metas)
+    elif action == "post_remove":
+        pass
+    elif action == "pre_add":
+        pass
+    elif action == "post_add":
+        add_count(metas)
 
 
 @receiver(pre_delete, sender=ContentsModel, dispatch_uid="delete_content")
