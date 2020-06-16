aa758980ce815c10af91a793030f59115f1af80d
mkdir700 <mkdir700@gmail.com>
2020-06-16 17:08:16 +0800

    注销relationships模块

--- apps/relationships/admin.py
+++ apps/relationships/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import RelationshipsModel
-
-
-@admin.register(RelationshipsModel)
-class RelationshipsAdmin(admin.ModelAdmin):
-    list_display = ['__str__']
+# from django.contrib import admin
+# from .models import RelationshipsModel
+#
+#
+# @admin.register(RelationshipsModel)
+# class RelationshipsAdmin(admin.ModelAdmin):
+#     list_display = ['__str__']
